5317bebe6ad816e0a2b3be9e74b578c6c696d362
RobLaran <160157653+RobLaran@users.noreply.github.com>
2025-09-24 20:59:29 +0800

    Added AuthMiddleware in Routes

--- routes/web.php
+++ routes/web.php
@@ -2,28 +2,41 @@
 use App\Core\Router;
 use App\Middleware\AuthMiddleware;
 
-Router::get('/', 'DashboardController@index', [[AuthMiddleware::class, 'check']]);
-Router::get('/dashboard', 'DashboardController@index');
-
 // User Routes
 Router::get('/auth/login', 'AuthController@loginUser');
 Router::post('/auth/login', 'AuthController@loginUser');
 
 // Admin Routes
-Router::get('/auth/login/admin', 'AuthController@loginAdminForm');
+Router::get('/auth/login/admin', 'AuthController@loginAdmin');
 Router::post('/auth/login/admin', 'AuthController@loginAdmin');
 
-// Book Routes
-Router::get('/books', 'BooksController@index');
-Router::get('/books/add', 'BooksController@create');
-Router::post('/books/add', 'BooksController@add');
-Router::get('/books/edit/{id}', 'BooksController@edit');
-Router::put('/books/update/{id}', 'BooksController@update');
-Router::delete('/books/delete/{id}', 'BooksController@delete');
+// Dashboard
+Router::get('/', 'DashboardController@index', [[AuthMiddleware::class, 'check']]);
+Router::get('/dashboard', 'DashboardController@index', [[AuthMiddleware::class, 'check']]);
 
-Router::get('/genres', 'GenresController@index');
-Router::get('/borrowers', 'BorrowersController@index');
-Router::get('/borrowed-books', 'BorrowedBooksController@index');
-Router::get('/returns', 'ReturnsController@index');
-Router::get('/overdue-books', 'OverdueBooks@index');
-Router::get('/reports', 'ReportsController@index');
+// Book Routes
+Router::get('/books', 'BooksController@index', [[AuthMiddleware::class, 'check']]);
+Router::get('/books/add', 'BooksController@create', [[AuthMiddleware::class, 'check']]);
+Router::post('/books/add', 'BooksController@add', [[AuthMiddleware::class, 'check']]);
+Router::get('/books/edit/{id}', 'BooksController@edit', [[AuthMiddleware::class, 'check']]);
+Router::put('/books/update/{id}', 'BooksController@update', [[AuthMiddleware::class, 'check']]);
+Router::delete('/books/delete/{id}', 'BooksController@delete', [[AuthMiddleware::class, 'check']]);
+
+// Genres
+Router::get('/genres', 'GenresController@index', [[AuthMiddleware::class, 'check']]);
+
+// Borrowers
+Router::get('/borrowers', 'BorrowersController@index', [[AuthMiddleware::class, 'check']]);
+
+// Borrowed Books
+Router::get('/borrowed-books', 'BorrowedBooksController@index', [[AuthMiddleware::class, 'check']]);
+
+// Returns
+Router::get('/returns', 'ReturnsController@index', [[AuthMiddleware::class, 'check']]);
+
+// Overdue
+Router::get('/overdue-books', 'OverdueBooks@index', [[AuthMiddleware::class, 'check']]);
+
+// Reports
+Router::get('/reports', 'ReportsController@index', [[AuthMiddleware::class, 'check']]);
+
